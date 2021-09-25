a583cd2aa0b7b1a1bc0a57d0b2249fe23d96354c
Badyan <Badyan@DESKTOP-BN9CQSJ>
2021-09-25 19:12:25 +0300

    Добавьте файлы проекта.

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOption<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Footbal", Price = 25},
+            new Product { Name = "Sggsdg", Price = 399},
+            new Product { Name = "Test3", Price = 10},
+        };
+    }
+}
